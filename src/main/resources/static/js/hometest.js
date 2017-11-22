cd477167ae5beb582515130bdd6163736fd5b00c
Ivpion <Ivanpiontkevich@gmail.com>
2017-11-23 00:21:08 +0200

    finish fix js(add pagination)

--- src/main/resources/static/js/hometest.js
+++ src/main/resources/static/js/hometest.js
@@ -70,105 +70,6 @@ function initToDoItem(itemData) {
     return li;
 }
 
-function getPagesInfo(pageNum) {
-    //  =>  /api/pages/{count: number} -> 5 (items per page) (total items = ceil(78 / 5)  )
-    //  <= {error: null, data: number} (means totalPages) <-16
-    //
-
-
-    $.ajax({
-        url: "/api/pages",
-        method: "GET",
-        data: {
-            count: $('#perPageCount').val()
-        },
-        success: function (response) {
-            if(response.error !== undefined){
-                alert("response error");
-                return;
-            }
-            var pagesCount = response.data;
-            var fragment = document.createDocumentFragment();
-            // process items in loop
-            for (var i = 1; i <= pagesCount; i++){
-                if(i < 4){
-                    fragment.appendChild(initPageElement(i));
-                } else {
-                    fragment.appendChild(initPageElement("..."));
-                    fragment.appendChild(initPageElement(pagesCount));
-                    break;
-                }
-            }
-
-
-            $(".pagination")
-                .empty()
-                .append(fragment)
-            ;
-            if(pageNum === undefined){
-                pageNum = 1;
-            }
-
-            readList(pageNum);
-        }
-
-    })
-}
-
-function initPageElement(numOfPage) {
-    var li = document.createElement("li");
-    li.innerHTML = numOfPage;
-    li.setAttribute("data-value", numOfPage);
-    if (numOfPage === "...") {
-        li.classList.add("nonActive");
-        return li;
-    }
-
-    li.addEventListener("click", function () {
-
-        var pageNo = this.getAttribute("data-value");
-        // todo: define if this number or text like "..."
-        if(pageNo === "..."){
-            //...
-        }
-        else {
-            readList(pageNo);
-        }
-    });
-
-    return li;
-}
-
-
-function readList(page) {
-
-
-    $.ajax({
-        url: '/api/list',
-        method: 'GET',
-        data: {
-            page: page,
-            count: 10,
-        },
-        success: function (data) {
-            document.getElementById("taskInput").value = "";
-            var fragment = document.createDocumentFragment();
-            if (data.error === undefined) {
-                for (var i = 0; i < data.data.items.length; i++) {
-                    var jsonObj = data.data.items[i];
-
-                    if (jsonObj !== undefined) {
-                        fragment.appendChild(initToDoItem(jsonObj));
-                    }
-                }
-                $("#tasksList").empty().append(fragment);
-            } else {
-                alert(data.error);
-            }
-        }
-    });
-}
-
 function newElement() {
 
     var inputValue = document.getElementById("taskInput").value;
@@ -200,36 +101,38 @@ function newElement() {
         }
     })
 }
-function invalidResp(resp) {
-    if(resp.error){
-        alert(resp.error)
-        location.reload();
+
+function createList(data) {
+    document.getElementById("taskInput").value = "";
+    var fragment = document.createDocumentFragment();
+    if (data.error === undefined) {
+        for (var i = 0; i < data.length; i++) {
+            var jsonObj = data[i];
+
+            if (jsonObj !== undefined) {
+                fragment.appendChild(initToDoItem(jsonObj));
+            }
+        }
+        $("#tasksList").empty().append(fragment);
+    } else {
+        alert(data.error);
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    // $('#perPageCount').on("change", function () {
-    //     getPagesInfo();
-    // })
-    // getPagesInfo();
-    //todo refactor code
-    //todo винести створення ліста в окремиий метод
-    //todo зробити робоций айтем перпейдж
-    //todo винести створення пагінатора в окремий метод
-    //
-
+function initPagination() {
     pagination = $('#paginator').pagination({
         dataSource: '/api/list', //[1, 2, 3, 4, 5, 6, 7, 195],
+        pageSize: $('#perPageCount').val(),
         alias: {
             pageNumber: 'page',
             pageSize: 'count'
         },
-        locator: function() {
+        locator: function () {
             // find data and return
             return 'data.items';
         },
         // totalNumber: 120,
-        totalNumberLocator: function(response){
+        totalNumberLocator: function (response) {
             // {
             //      data: {
             //          items: [ {...}, ....],
@@ -243,18 +146,28 @@ document.addEventListener("DOMContentLoaded", function () {
             // template method of yourself
             // var html = template(data);
             // dataContainer.html(html);
-            console.log(data, pagination);
+            console.log(data);
+            console.log(pagination);
             console.log(pagination.pageNumber);
-            readList(pagination.pageNumber);
+            createList(data);
         }
     });
+}
+
+function invalidResp(resp) {
+    if(resp.error){
+        alert(resp.error)
+        location.reload();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", initPagination);
+     $('#perPageCount').on("change", function () {
+        initPagination();
+    })
 
 
 
 
 
-
-
-
-});
 
